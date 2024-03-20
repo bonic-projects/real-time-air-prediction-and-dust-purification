25bef612bedbea22d355699e65f19e145d48732a
Azhar <mohdazharsm@gmil.com>
2024-03-20 14:27:05 +0530

    hardware code

---
+++ hardware/arduino/esp/esp.ino
@@ -0,0 +1,144 @@
+//Firebase=================================================
+//WiFi
+//Firebase
+#include <Arduino.h>
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+// Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "Autobonics_4G"
+#define WIFI_PASSWORD "autobonics@27"
+// For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
+/* 2. Define the API Key */
+#define API_KEY "AIzaSyBvKqPRgWXhX-a_UIXwgaYAoumwrmeSfzg"
+/* 3. Define the RTDB URL */
+#define DATABASE_URL "https://air-guard-127e8-default-rtdb.asia-southeast1.firebasedatabase.app/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+/* 4. Define the user Email and password that alreadey registerd or added in your project */
+#define USER_EMAIL "device@gmail.com"
+#define USER_PASSWORD "12345678"
+// Define Firebase Data object
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+unsigned long sendDataPrevMillis = 0;
+// Variable to save USER UID
+String uid;
+//Databse
+String path;
+
+//===============================
+#include "DHT.h"
+#define DHT11PIN 33
+DHT dht(DHT11PIN, DHT11);
+
+float humi;
+float temp;
+  
+void setup()
+{
+  
+  Serial.begin(115200);
+/* Start the DHT11 Sensor */
+  dht.begin();
+
+  ///=============
+  //WIFI
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  unsigned long ms = millis();
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+
+  //FIREBASE
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  // Limit the size of response payload to be collected in FirebaseData
+  fbdo.setResponseSize(2048);
+
+  Firebase.begin(&config, &auth);
+
+  // Comment or pass false value when WiFi reconnection will control by your code or third party library
+  Firebase.reconnectWiFi(true);
+
+  Firebase.setDoubleDigits(5);
+
+  config.timeout.serverResponse = 10 * 1000;
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(uid);
+
+  path = "devices/" + uid + "/reading";
+}
+
+
+void updateData(){
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 4000 || sendDataPrevMillis == 0))
+  {
+    sendDataPrevMillis = millis();
+    FirebaseJson json;
+    json.set("temp", temp);
+    json.set("humi", humi);
+    // json.set("speed", speed_kmph);
+    // json.set("temp", temp);
+    // json.set("sos", isSos);
+    // json.set("acl_x", acl_x);
+    // json.set("acl_y", acl_y);
+    // json.set("acl_z", acl_z);
+    // json.set("gyro_x", gyro_x);
+    // json.set("gyro_y", gyro_y);
+    // json.set("gyro_z", gyro_z);
+    json.set(F("ts/.sv"), F("timestamp"));
+    // Serial.printf("Set json... %s\n", Firebase.RTDB.set(&fbdo, path.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
+    Serial.printf("Set data with timestamp... %s\n", Firebase.setJSON(fbdo, path.c_str(), json) ? fbdo.to<FirebaseJson>().raw() : fbdo.errorReason().c_str());
+    Serial.println(""); 
+  }
+}
+
+void loop()
+{
+  readDht();
+  updateData();
+}
+
+
+void readDht(){
+  humi = dht.readHumidity();
+  temp = dht.readTemperature();
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print("ºC ");
+  Serial.print("Humidity: ");
+  Serial.println(humi);
+  delay(1000);
+}
